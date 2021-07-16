b28b6a37066cf136f92d8f2e3dc4a350dda5d4c4
GoutamVerma <goutamverma84009@gmail.com>
2021-07-16 10:49:29 +0530

    initial commit

---
+++ app/src/main/java/com/lglab/goutam/simple_cms/account/ConstantsLogInLogOut.java
@@ -0,0 +1,5 @@
+package com.lglab.goutam.simple_cms.account;
+
+public enum ConstantsLogInLogOut {
+    LOGIN, IS_LOGIN;
+}
---
+++ app/src/main/java/com/lglab/goutam/simple_cms/connection/ILGConnection.java
@@ -0,0 +1,5 @@
+package com.lglab.goutam.simple_cms.connection;
+
+public interface ILGConnection {
+    void setStatus(short status);
+}
